f74f1ac0bcb48adb7727bb22fb95dce3f4c9b861
Max Korsakov <Maksim_Karsakou1epam.com>
2020-04-17 19:42:10 +0300

    update styles with pseudoselectors

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ButtonComponent implements OnInit {
 
